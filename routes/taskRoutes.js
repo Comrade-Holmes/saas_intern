4aed7995aedf1c505da8fed6476f846ca9be3485
Comrade-Holmes <santhoshravi1903@gmail.com>
2026-06-08 22:04:40 +0530

    Update taskRoutes.js

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,57 +1,54 @@
+const express = require("express");
+const Task = require("../models/Task");
+const auth = require("../middleware/authMiddleware");
 
-const router=require("express").Router();
-const Task=require("../models/Task");
-const auth=require("../middleware/authMiddleware");
+const router = express.Router();
 
-router.post("/tasks",auth,async(req,res)=>{
+router.get("/", auth, async (req, res) => {
+  const tasks = await Task.find({
+    userId: req.user.id,
+  });
 
-const task=new Task({
-title:req.body.title,
-description:req.body.description,
-userId:req.user.id
+  res.json(tasks);
 });
 
-await task.save();
+router.post("/", auth, async (req, res) => {
+  const { title, description } = req.body;
 
-res.json({
-message:"Task Added"
+  if (!title) {
+    return res
+      .status(400)
+      .json({ message: "Title is required" });
+  }
+
+  const task = new Task({
+    title,
+    description,
+    userId: req.user.id,
+  });
+
+  await task.save();
+
+  res.status(201).json(task);
 });
 
+router.delete("/:id", auth, async (req, res) => {
+  const task = await Task.findOne({
+    _id: req.params.id,
+    userId: req.user.id,
+  });
+
+  if (!task) {
+    return res
+      .status(404)
+      .json({ message: "Task not found" });
+  }
+
+  await task.deleteOne();
+
+  res.json({
+    message: "Task deleted",
+  });
 });
 
-router.get("/tasks",auth,async(req,res)=>{
-
-const tasks=await Task.find({
-userId:req.user.id
-});
-
-res.json(tasks);
-
-});
-
-router.put("/tasks/:id",auth,async(req,res)=>{
-
-await Task.findByIdAndUpdate(
-req.params.id,
-req.body
-);
-
-res.json({
-message:"Task Updated"
-});
-
-});
-
-router.delete("/tasks/:id",auth,async(req,res)=>{
-
-await Task.findByIdAndDelete(
-req.params.id
-);
-
-res.json({
-message:"Task Deleted"
-});
-
-});
-
-module.exports=router;
+module.exports = router;
